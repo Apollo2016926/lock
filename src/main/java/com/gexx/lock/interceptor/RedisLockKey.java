7c0bbd236382623e2bde45b45c9be7ca3d6c39d1
Eden <944644308@qq.com>
2023-03-13 19:18:49 +0800

    Update RedisLockKey.java

--- src/main/java/com/gexx/lock/interceptor/RedisLockKey.java
+++ src/main/java/com/gexx/lock/interceptor/RedisLockKey.java
@@ -3,7 +3,7 @@ package com.gexx.lock.interceptor;
 import java.lang.annotation.*;
 
 /*
-默认30 秒过期，过期后会自动续期
+  注解版分布式锁，默认30 秒过期，过期后会自动续期
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
